e51c01fd3d1ccf5d665a40663ec692ab10bd82d7
coderaiser <coderaiser@cloudcmd.io>
2020-03-25 17:39:28 +0200

    chore(madrun) lint

--- .madrun.js
+++ .madrun.js
@@ -3,7 +3,7 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'lint': () => 'putout lib madrun.js',
+    'lint': () => 'putout lib .madrun.js',
     'fix:lint': () => run('lint', '--fix'),
 };
 
