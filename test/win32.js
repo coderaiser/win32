dcd477155ea0ff60680c0e1af800bfebef5075e5
coderaiser <coderaiser@cloudcmd.io>
2021-01-25 20:39:56 +0200

    fix(win32) read: root

--- test/win32.js
+++ test/win32.js
@@ -51,3 +51,41 @@ test('win32: read: stringify with spaces', async (t) => {
     t.equal(result, expected);
     t.end();
 });
+
+test('win32: read: root', async (t) => {
+    const read = stub().returns({
+        files: [{
+            name: 'hello.txt',
+            type: 'file',
+            size: '5b',
+            date: '--.--.----',
+            mode: '--- --- ---',
+            owner: 0,
+        }],
+    });
+    
+    mockRequire('redzip', {
+        read,
+    });
+    
+    const win32 = reRequire('..');
+    const result = await pullout(await win32.read('/hello/world', {
+        root: '/hello',
+    }));
+    const expected = stringify({
+        files: [{
+            name: 'hello.txt',
+            type: 'file',
+            size: '5b',
+            date: '--.--.----',
+            mode: '--- --- ---',
+            owner: 0,
+        }],
+        path: '/world',
+    }, null, 4);
+    
+    stopAll();
+    
+    t.equal(result, expected);
+    t.end();
+});
