a002bbb84ce5b9899dbcb952df218c67ebb4703a
coderaiser <coderaiser@cloudcmd.io>
2021-01-25 20:32:19 +0200

    fix(win32) stringify

--- test/win32.js
+++ test/win32.js
@@ -1,12 +1,53 @@
 'use strict';
 
-const test = require('supertape');
+const {stub, test} = require('supertape');
 const pullout = require('pullout');
+const mockRequire = require('mock-require');
+
 const {unicodify} = require('..');
+
+const {reRequire, stopAll} = mockRequire;
+const {stringify} = JSON;
 
 test('win32: unicodify: linux', async (t) => {
     const result = await pullout(unicodify('hello'));
     
     t.equal(result, 'hello');
+    t.end();
+});
+
+test('win32: read: stringify with spaces', async (t) => {
+    const read = stub().returns({
+        files: [{
+            name: 'hello.txt',
+            type: 'file',
+            size: '5b',
+            date: '--.--.----',
+            mode: '--- --- ---',
+            owner: 0,
+        }],
+    });
+    
+    mockRequire('redzip', {
+        read,
+    });
+    
+    const win32 = reRequire('..');
+    const result = await pullout(await win32.read('.'));
+    const expected = stringify({
+        files: [{
+            name: 'hello.txt',
+            type: 'file',
+            size: '5b',
+            date: '--.--.----',
+            mode: '--- --- ---',
+            owner: 0,
+        }],
+        path: '.',
+    }, null, 4);
+    
+    stopAll();
+    
+    t.equal(result, expected);
     t.end();
 });
