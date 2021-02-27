7eec67525665b4511108753491a6f385402fae13
coderaiser <coderaiser@cloudcmd.io>
2021-02-27 20:08:47 +0200

    chore(win32) lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -8,7 +8,7 @@ export default {
     'test': () => 'tape test/*.js',
     'watch:coverage': () => run('watcher', 'npm run coverage'),
     'watch:test': () => run('watcher', 'npm test'),
-    'report': () => 'c8 report --reporter=text-lcov | coveralls',
+    'report': () => 'c8 report --reporter=lcov',
     'coverage': () => 'c8 npm test',
 };
 
